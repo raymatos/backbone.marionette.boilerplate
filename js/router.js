24cd01951871a6aba65386d0efdabc71694c1ccd
Raymond Matos <ray.matos@medialets.com>
2013-02-06 23:51:52 -0500

    use require the right way, inside the event.
    Signed-off-by: Raymond Matos <ray.matos@medialets.com>

--- js/router.js
+++ js/router.js
@@ -5,10 +5,8 @@ define([
   'backbone',
   'marionette',
   'views/home/HomeView',
-  'views/projects/ProjectsView',
-  'views/contributors/ContributorsView',
   'views/footer/FooterView'
-], function($, _, Backbone, Marionette, HomeView, ProjectsView, ContributorsView, FooterView) {
+], function($, _, Backbone, Marionette, HomeView, FooterView) {
   
   var AppRouter = Backbone.Router.extend({
     routes: {
@@ -26,22 +24,26 @@ define([
     var app_router = new AppRouter;
     
     app_router.on('route:showProjects', function(){
-   
-        // Call render on the module we loaded in via the dependency array
-        var projectsView = new ProjectsView();
-        projectsView.render();
-
+   		require(['views/projects/ProjectsView',],
+	        function(ProjectsView){
+	          	// Call render on the module we loaded in via the dependency array
+        		var projectsView = new ProjectsView();
+        		projectsView.render();
+	       }
+	    );    
     });
 
     app_router.on('route:showContributors', function () {
-    
-        // Like above, call render but know that this view has nested sub views which 
-        // handle loading and displaying data from the GitHub API  
-        var contributorsView = new ContributorsView();
+    	require(['views/contributors/ContributorsView',],
+    		function(ContributorsView){
+    			// Like above, call render but know that this view has nested sub views which 
+        		// handle loading and displaying data from the GitHub API  
+        		var contributorsView = new ContributorsView();
+    		}
+    	);
     });
 
     app_router.on('route:defaultAction', function (actions) {
-     
        // We have no matching route, lets display the home page 
         var homeView = new HomeView();
         homeView.render();
